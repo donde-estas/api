ae50979fec5ef0341a96dfa24f4ee0ba09136d59
Daniel Leal <dlleal@uc.cl>
2019-10-22 18:23:29 -0300

    Added initial post for a missing person

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import json
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from helpers.keys import generate_random_key
 
 app = Flask(__name__)
 
@@ -21,6 +22,30 @@ def hello():
 def missing():
     try:
         return jsonify({'success': True, 'payload': Person.query.all()})
+    except Exception as error:
+        return jsonify({'success': False, 'payload': str(error)})
+
+
+@app.route("/missing", methods=['POST'])
+def missing():
+    """Creates a missing person in the database."""
+    try:
+        first_name = request.args.get('first_name')
+        last_name = request.args.get('last_name')
+        phone_number = request.args.get('phone_number')
+        plain_key = generate_random_key()
+
+        person = Person(first_name, last_name, plain_key)
+
+        db.session.add(person)
+        db.session.commit()
+        return jsonify({
+            'success': True,
+            'payload': {
+                'plain_key': plain_key,
+                'person': person.serialize()
+            }
+        })
     except Exception as error:
         return jsonify({'success': False, 'payload': str(error)})
 
