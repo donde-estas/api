2247c819bb1b61f3e87d011807ff1e29c7aa2a1e
Daniel Leal <37158127+daleal@users.noreply.github.com>
2019-10-22 18:59:43 -0300

    Add person (#3)
    * Now all responses return a boolean under the key 'success' to indicate if the request was successful or not
    
    * Now the serialized version of Person includes its creation date
    
    * Added initial post for a missing person
    
    * Corrected key management methods and variable names
    
    * Changed colisioning names and get_all return
    
    * Added method to create a new missing person

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import json
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from helpers.keys import generate_random_key
 
 app = Flask(__name__)
 
@@ -18,11 +19,40 @@ def hello():
 
 
 @app.route("/missing", methods=['GET'])
-def missing():
+def get_all_missing():
     try:
-        return jsonify(Person.query.all())
+        return jsonify({
+            'success': True,
+            'payload': [x.serialize() for x in Person.query.all()]
+        })
     except Exception as error:
-        return jsonify({'error': str(error)})
+        return jsonify({'success': False, 'payload': str(error)})
+
+
+@app.route("/missing", methods=['POST'])
+def create_new_missing():
+    """Creates a missing person in the database."""
+    try:
+        first_name = request.args.get('first_name')
+        last_name = request.args.get('last_name')
+        missing_number = request.args.get('missing_number')
+        contact_number = request.args.get('contact_number')
+        plain_key = generate_random_key()
+
+        person = Person(first_name, last_name, plain_key)
+
+        db.session.add(person)
+        db.session.commit()
+        return jsonify({
+            'success': True,
+            'payload': {
+                'plain_key': plain_key,
+                'person': person.serialize()
+            }
+        })
+    except Exception as error:
+        return jsonify({'success': False, 'payload': str(error)})
+
 
 
 if __name__ == '__main__':
