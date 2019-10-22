592e1af8c2e87873d7556dd364c2c93d5865a873
Daniel Leal <dlleal@uc.cl>
2019-10-22 16:39:28 -0300

    Added methods to manage keys and security

---
+++ helpers/keys.py
@@ -0,0 +1,37 @@
+"""This module contains every key helper needed inside the API."""
+
+
+import os
+import string
+from secrets import choice
+import bcrypt
+
+
+LOG_ROUNDS = os.environ.get("LOG_ROUNDS") or 12
+PASSWORD_LENGTH = os.environ.get("PASSWORD_LENGTH") or 16
+ALPHABET = string.ascii_letters + string.digits
+
+
+def generate_random_key():
+    """
+    Generates a random alphanumeric password of length :PASSWORD_LENGTH
+    with at least one lowercase character, one uppercase character and
+    at least 4 digits.
+    """
+    # Based on the secrets module documentation
+    while True:
+        key = ''.join(choice(ALPHABET) for _ in range(int(PASSWORD_LENGTH)))
+        if (any(c.islower() for c in key)
+                and any(c.isupper() for c in key)
+                and sum(c.isdigit() for c in key) >= 4):
+            return key
+
+
+def generate_key_digest(plain_key):
+    """Digests a key using :LOG_ROUNDS log rounds."""
+    return bcrypt.hashpw(plain_text_password, bcrypt.gensalt(int(LOG_ROUNDS)))
+
+
+def check_key(plain_key, key_digest):
+    """Checks if :plain_key is the key to :key_digest"""
+    return bcrypt.checkpw(plain_key, key_digest)
