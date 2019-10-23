161bc18fb98df9970a829790cefcb28c28a85ff8
Daniel Leal <dlleal@uc.cl>
2019-10-23 14:25:14 -0300

    Refactored mailing strategy to have a modular method to send one mail and to use it twice

--- helpers/mailer.py
+++ helpers/mailer.py
@@ -1,33 +1,30 @@
+import os
 from requests import post
-from pprint import pprint
-from flask import render_template
-from templates import (mailer_link, skeleton, body_template, 
-                        informant1, informant2, 
-                        missing1, missing2)
 
-def send_mails(mail_informant:str, mail_missing:str, name_informant:str, name_missing:str):
+
+def send_mail(mail: str, message: str):
     """
-    Function that sends mails through the Mailer API
-        :param mail_informant:str: Informant's E-Mail Address
-        :param mail_missing:str: Missing Person's E-Mail Address
-        :param name_informant:str: Informant's Name
-        :param name_missing:str: Missing Person's Name
+    Function that sends a mail through the Mailer API
+    :param mail: str => Email address to send the email
+    :param message: str => Message to send
     """
-    id1 = informant1.format(nombre=name_missing)
-    md1 = missing1.format(nombre=name_informant)
-    body_informant = body_template.format(nombre=name_informant, descripcion1=id1, descripcion2=informant2)
-    body_missing = body_template.format(nombre=name_missing, descripcion1=md1, descripcion2=missing2)
-    r_i = post(mailer_link, params={
-        'email': mail_informant,
-        'body': skeleton.replace("{body}", body_informant),
-        'key': 'j40BZwaEcW35qLpcQbWv',
+    status = post(f'{os.environ.get("MAIL_SENDER_API_URL")}/send', params={
+        'email': mail,
+        'body': message,
+        'key': os.environ.get('MAIL_SENDER_API_KEY'),
         'title': "Presunto Desaparecido Registrado"
     })
-    print(r_i)
-    r_m = post(mailer_link, params={
-        'email': mail_missing,
-        'body': skeleton.replace("{body}", body_missing),
-        'key': 'j40BZwaEcW35qLpcQbWv',
-        'title': "Usted ha sido registrad@ cómo posible desaparecid@"
-    })
-    print(r_m)
+    return status
+
+
+def complete_template_body(body, parameters):
+    """Replaces the :parameters in :body."""
+    return body.format(**parameters)
+
+
+def dispatch_mail(mail, template, body, style, parameters):
+    """Completes the template and sends mail."""
+    body = complete_template_body(body, parameters)
+    message = template.format(style=style, body=body)
+    status = send_mail(mail, message)
+    return status
