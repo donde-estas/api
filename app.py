fd15e51bd138953cb8471bca74d2339a85a08429
Daniel Leal <dlleal@uc.cl>
2019-10-23 01:20:47 -0300

    Changed some URLs and added a method to the client to see all found persons

--- app.py
+++ app.py
@@ -31,74 +31,6 @@ def get_all_missing():
         return jsonify({'success': False, 'payload': str(error)}), 503
 
 
-@app.route("/missing", methods=['POST'])
-def create_new_missing():
-    """Creates a missing person in the database."""
-    try:
-        first_name = request.args.get('first_name')
-        last_name = request.args.get('last_name')
-        missing_number = request.args.get('missing_number')
-        contact_number = request.args.get('contact_number')
-        plain_key = generate_random_key()
-
-        person = Person(first_name, last_name, plain_key)
-
-        db.session.add(person)
-        db.session.commit()
-        return jsonify({
-            'success': True,
-            'payload': {
-                'plain_key': plain_key,
-                'person': person.serialize()
-            }
-        }), 200
-    except Exception as error:
-        return jsonify({'success': False, 'payload': str(error)}), 503
-
-
-@app.route("/missing/<int:id_>", methods=['GET'])
-def get_missing(id_):
-    try:
-        person = Person.query.filter_by(id=id_).first()
-        if not person:
-            return jsonify({
-                'success': False,
-                'payload': 'Person does not exist in the database (invalid id)'
-            }), 404
-
-        return jsonify({
-            'success': True,
-            'payload': person.serialize()
-        }), 200
-    except Exception as error:
-        return jsonify({'success': False, 'payload': str(error)}), 503
-
-
-@app.route("/missing/<int:id_>", methods=['DELETE'])
-def delete_missing(id_):
-    try:
-        person = Person.query.filter_by(id=id_).first()
-        plain_key = request.args.get('plain_key')
-        if not person:
-            return jsonify({
-                'success': False,
-                'payload': 'Person does not exist in the database (invalid id)'
-            }), 404
-        if not person.check_plain_key(plain_key):
-            return jsonify({
-                'success': False,
-                'payload': 'Invalid auth key'
-            }), 401
-        db.session.delete(person)
-        db.session.commit()
-        return jsonify({
-            'success': True,
-            'payload': "Person removed successfully"
-        }), 200
-    except Exception as error:
-        return jsonify({'success': False, 'payload': str(error)}), 503
-
-
 @app.route("/missing/<int:id_>", methods=['PATCH'])
 def find_missing(id_):
     try:
@@ -140,6 +72,74 @@ def get_all_found():
         return jsonify({
             'success': True,
             'payload': [x.serialize() for x in Person.query.all() if x.found]
+        }), 200
+    except Exception as error:
+        return jsonify({'success': False, 'payload': str(error)}), 503
+
+
+@app.route("/person", methods=['POST'])
+def create_person():
+    """Creates a missing person in the database."""
+    try:
+        first_name = request.args.get('first_name')
+        last_name = request.args.get('last_name')
+        missing_number = request.args.get('missing_number')
+        contact_number = request.args.get('contact_number')
+        plain_key = generate_random_key()
+
+        person = Person(first_name, last_name, plain_key)
+
+        db.session.add(person)
+        db.session.commit()
+        return jsonify({
+            'success': True,
+            'payload': {
+                'plain_key': plain_key,
+                'person': person.serialize()
+            }
+        }), 200
+    except Exception as error:
+        return jsonify({'success': False, 'payload': str(error)}), 503
+
+
+@app.route("/person/<int:id_>", methods=['GET'])
+def get_person(id_):
+    try:
+        person = Person.query.filter_by(id=id_).first()
+        if not person:
+            return jsonify({
+                'success': False,
+                'payload': 'Person does not exist in the database (invalid id)'
+            }), 404
+
+        return jsonify({
+            'success': True,
+            'payload': person.serialize()
+        }), 200
+    except Exception as error:
+        return jsonify({'success': False, 'payload': str(error)}), 503
+
+
+@app.route("/person/<int:id_>", methods=['DELETE'])
+def delete_person(id_):
+    try:
+        person = Person.query.filter_by(id=id_).first()
+        plain_key = request.args.get('plain_key')
+        if not person:
+            return jsonify({
+                'success': False,
+                'payload': 'Person does not exist in the database (invalid id)'
+            }), 404
+        if not person.check_plain_key(plain_key):
+            return jsonify({
+                'success': False,
+                'payload': 'Invalid auth key'
+            }), 401
+        db.session.delete(person)
+        db.session.commit()
+        return jsonify({
+            'success': True,
+            'payload': "Person removed successfully"
         }), 200
     except Exception as error:
         return jsonify({'success': False, 'payload': str(error)}), 503
