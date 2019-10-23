54735415ec781e53c0e38bb059eb718ae0ffd8f3
Daniel Leal <dlleal@uc.cl>
2019-10-22 23:00:45 -0300

    Added error checking to the ghost script

--- ghost_client.py
+++ ghost_client.py
@@ -53,7 +53,16 @@ if __name__ == "__main__":
         "get_missing":        get_missing,
         "delete_missing":     delete_missing
     }
-    RESPONSE = COMMANDS[sys.argv[1]](LINK, *sys.argv[2:])
+    try:
+        RESPONSE = COMMANDS[sys.argv[1]](LINK, *sys.argv[2:])
+    except IndexError:
+        if len(sys.argv) <= 1:
+            print("\n\n\n\tInvalid script call. To use the script, run:")
+            print("\tpython3 ghost_client.py command *args")
+            print("\tWhere *args are the arguments separated by space and "
+                  "command corresponds to one of the following:")
+            print(f'\t\t{", ".join(COMMANDS.keys())}\n\n')
+            sys.exit(1)
     try:
         print(json.dumps(RESPONSE, indent=2, sort_keys=False))
     except TypeError:
