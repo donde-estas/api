f61414285c5ccc62828b63e66a81b5cc0227b37a
Daniel Leal <37158127+daleal@users.noreply.github.com>
2019-10-23 01:24:19 -0300

    Separate missing from found (#15)
    * Now the missing route only returns missing people
    
    * Added method to get everyone found
    
    * Changed some URLs and added a method to the client to see all found persons
    
    * Fixed a typo

--- ghost_client.py
+++ ghost_client.py
@@ -19,7 +19,7 @@ def get_all_missing(link):
     return requests.get(f'{link}/missing').json()
 
 
-def create_new_missing(link, first_name, last_name, missing_n, contact_n):
+def create_person(link, first_name, last_name, missing_n, contact_n):
     """Creates a new missing person in the database."""
     payload = {
         "first_name": first_name,
@@ -27,20 +27,20 @@ def create_new_missing(link, first_name, last_name, missing_n, contact_n):
         "missing_number": missing_n,
         "contact_number": contact_n
     }
-    return requests.post(f'{link}/missing', params=payload).json()
+    return requests.post(f'{link}/person', params=payload).json()
 
 
-def get_missing(link, id_):
+def get_person(link, id_):
     """Gets a specific missing person."""
-    return requests.get(f'{link}/missing/{id_}').json()
+    return requests.get(f'{link}/person/{id_}').json()
 
 
-def delete_missing(link, id_, plain_key):
+def delete_person(link, id_, plain_key):
     """Deletes a specific missing person."""
     payload = {
         "plain_key": plain_key
     }
-    return requests.delete(f'{link}/missing/{id_}', params=payload).json()
+    return requests.delete(f'{link}/person/{id_}', params=payload).json()
 
 
 def find_missing(link, id_, plain_key):
@@ -51,16 +51,22 @@ def find_missing(link, id_, plain_key):
     return requests.patch(f'{link}/missing/{id_}', params=payload).json()
 
 
+def get_all_found(link):
+    """Gets all found persons."""
+    return requests.get(f'{link}/found').json()
+
+
 if __name__ == "__main__":
     LINK = "http://0.0.0.0:5000"
     # LINK = "http://donde-estas.herokuapp.com"
     COMMANDS = {
-        "wake_up":            wake_up,
-        "get_all_missing":    get_all_missing,
-        "create_new_missing": create_new_missing,
-        "get_missing":        get_missing,
-        "delete_missing":     delete_missing,
-        "find_missing":       find_missing
+        "wake_up":          wake_up,
+        "get_all_missing":  get_all_missing,
+        "create_person":    create_person,
+        "get_person":       get_person,
+        "delete_person":    delete_person,
+        "find_missing":     find_missing,
+        "get_all_found":    get_all_found
     }
     try:
         RESPONSE = COMMANDS[sys.argv[1]](LINK, *sys.argv[2:])
