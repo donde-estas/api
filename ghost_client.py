9cc3ca810796501a63977cd14e442c7976bd9841
Daniel Leal <dlleal@uc.cl>
2019-10-22 21:59:19 -0300

    Added method to call endpoint in the server to delete a person

--- ghost_client.py
+++ ghost_client.py
@@ -11,17 +11,12 @@ import requests
 
 def wake_up(link):
     """Wakes up the server by asking for its root page."""
-    return requests.get(link)
+    return requests.get(f'{link}/')
 
 
 def get_all_missing(link):
     """Gets all missing persons."""
-    return requests.get(link + 'missing').json()
-
-
-def get_missing(link, id_):
-    """Gets a specific missing person."""
-    return requests.get(link + 'missing' + '/' + str(id_)).json()
+    return requests.get(f'{link}/missing').json()
 
 
 def create_new_missing(link, first_name, last_name, missing_n, contact_n):
@@ -32,17 +27,31 @@ def create_new_missing(link, first_name, last_name, missing_n, contact_n):
         "missing_number": missing_n,
         "contact_number": contact_n
     }
-    return requests.post(link + 'missing', params=payload).json()
+    return requests.post(f'{link}/missing', params=payload).json()
+
+
+def get_missing(link, id_):
+    """Gets a specific missing person."""
+    return requests.get(f'{link}/missing/{id_}').json()
+
+
+def delete_missing(link, id_, plain_key):
+    """Deletes a specific missing person."""
+    payload = {
+        "plain_key": plain_key
+    }
+    return requests.delete(f'{link}/missing/{id_}', params=payload).json()
 
 
 if __name__ == "__main__":
-    LINK = "http://0.0.0.0:5000/"
-    # LINK = "http://donde-estas.herokuapp.com/"
+    LINK = "http://0.0.0.0:5000"
+    # LINK = "http://donde-estas.herokuapp.com"
     COMMANDS = {
         "wake_up":            wake_up,
         "get_all_missing":    get_all_missing,
+        "create_new_missing": create_new_missing,
         "get_missing":        get_missing,
-        "create_new_missing": create_new_missing
+        "delete_missing":     delete_missing
     }
     RESPONSE = COMMANDS[sys.argv[1]](LINK, *sys.argv[2:])
     try:
