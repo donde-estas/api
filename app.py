e3f007500f09eb666ab839e94a9928353fdad4fc
Daniel Leal <dlleal@uc.cl>
2019-10-23 23:55:31 -0300

    The person creator now saves the mails in the person model

--- app.py
+++ app.py
@@ -109,7 +109,8 @@ def create_person():
             raise InvalidMailError(contact_mail)
         plain_key = generate_random_key()
 
-        person = Person(first_name, last_name, plain_key)
+        person = Person(first_name, last_name, missing_mail,
+                        contact_mail, plain_key)
 
         # Generate mails
         mail_args = {
@@ -125,7 +126,7 @@ def create_person():
             templates.template,
             templates.initial_missing_body,
             templates.default_style,
-            templates.find_person_button.format(found_link=found_link, 
+            templates.find_person_button.format(found_link=found_link,
                                                 message="¡Estoy Bien!"),
             mail_args)
         contact_s = dispatch_mail(
@@ -133,7 +134,7 @@ def create_person():
             templates.template,
             templates.initial_contact_body,
             templates.default_style,
-            templates.find_person_button.format(found_link=found_link, 
+            templates.find_person_button.format(found_link=found_link,
                                                 message="¡Apareció!"),
             mail_args)
 
