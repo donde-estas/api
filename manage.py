16449119ef691fbf56e9ca245280a430159d2b37
Daniel Leal <dlleal@uc.cl>
2019-10-24 01:03:42 -0300

    Preliminary manage.py formation

--- manage.py
+++ manage.py
@@ -1,16 +1,25 @@
 import os
 from flask_script import Server, Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask_dbseeder import Seeder, SeederCommand
 
+from seeds import PersonSeeder
 from app import app, db
 
 
-migrate = Migrate(app, db)
-manager = Manager(app)
-manager.add_command("runserver", Server(
-    host='0.0.0.0', port=os.environ.get('PORT')))
+# Create objects
+seeder = Seeder(app, db)    # Create seeder
+migrate = Migrate(app, db)  # Create migrator
+manager = Manager(app)      # Create manager
+
+# Add commands
+manager.add_command('seed', SeederCommand)
+seeder.add_seeds([PersonSeeder])
 
 manager.add_command('db', MigrateCommand)
+
+manager.add_command("runserver", Server(
+    host='0.0.0.0', port=os.environ.get('PORT')))
 
 
 if __name__ == '__main__':
